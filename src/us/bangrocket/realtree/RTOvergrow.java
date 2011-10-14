13c6b6e9e01442c3801446513f4249df9117f3e4
Joshua Heidorn <heidorjm@live.com>
2011-10-13 22:18:45 -0400

    nightly commit
    more overgrow experiments, limited amounts of success

--- src/us/bangrocket/realtree/RTOvergrow.java
+++ src/us/bangrocket/realtree/RTOvergrow.java
@@ -1,8 +1,11 @@
 package us.bangrocket.realtree;
 
+import java.util.LinkedList;
+
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 
 public class RTOvergrow 
 {
@@ -13,6 +16,9 @@ public class RTOvergrow
         plugin = instance;
 	}
 	
+	private LinkedList<Block> validBlocks = new LinkedList<Block>();
+	private LinkedList<Block> invalidBlocks = new LinkedList<Block>();
+	
 	private void loopThrough(Block block, Location loc1, Location loc2)
 	{
 	    int minx = Math.min(loc1.getBlockX(), loc2.getBlockX()),
@@ -21,43 +27,103 @@ public class RTOvergrow
 	        maxx = Math.max(loc1.getBlockX(), loc2.getBlockX()),
 	        maxy = Math.max(loc1.getBlockY(), loc2.getBlockY()),
 	        maxz = Math.max(loc1.getBlockZ(), loc2.getBlockZ());
-	    
+
 	    for(int x = minx; x<=maxx;x++)
 	    {
 	        for(int y = miny; y<=maxy;y++)
 	        {
 	            for(int z = minz; z<=maxz;z++)
 	            {
-	            	Block test = block.getWorld().getBlockAt(x, y, z);
-	                if (test.getType().equals(Material.GRASS) || test.getType().equals(Material.DIRT))
-	                	test.setType(Material.AIR);
-	                else
-	                	test.setType(Material.GRASS);
+	            	
+	            	Block tempBlock = block.getWorld().getBlockAt(x,y,z);
+	            	
+	            	if ((tempBlock.getType().equals(Material.GRASS) || 
+	            		(tempBlock.getType().equals(Material.DIRT)) ||
+	            		(tempBlock.getType().equals(Material.AIR))))
+	            	{
+	            		validBlocks.push(tempBlock);
+	            	}
 	            }
 	        }
 	    }
 	    block.setType(Material.LOG);
+	    
+	    //if validBlocks.containsAll(c)
+	    
+	    while (!validBlocks.isEmpty())
+	    {
+	    	Block checkBlock =  validBlocks.pop();
+	    
+	    	if (!invalidBlocks.contains(checkBlock) || !invalidBlocks.contains(checkBlock.getRelative(BlockFace.UP)))
+	    	{
+	    		if (checkBlock.getType().equals(Material.GRASS) || checkBlock.getType().equals(Material.DIRT))
+	    			{
+	    				if (checkBlock.getRelative(BlockFace.UP).getType().equals(Material.AIR))
+	    				{
+	    					if (!nearbyMaterial(checkBlock.getRelative(BlockFace.UP), Material.SAPLING))
+	    						checkBlock.getRelative(BlockFace.UP).setType(Material.SAPLING);
+	    				}
+	    			
+	    			}
+	    	}
+	    	
+	    } 
+	    
+	    invalidBlocks.clear();
+	    validBlocks.clear();
+	    
+	}
+
+	private boolean isTouching(Block block) {
+
+		for(int z = -1; z <= 1; z++)
+		{
+		  for(int x = -1; x <= 1; x++)
+		  {
+		    for(int y = -1; y <= 1; y++)
+		    {
+		    	invalidBlocks.add(block.getWorld().getBlockAt(x, y, z));
+		    }
+		  }
+		}
+
+		return true;
+	}
+
+	private boolean nearbyMaterial(Block block, Material mat)
+	{
+		
+		if 	((block.equals(mat)) ||
+			(block.getRelative(BlockFace.UP).getType().equals(mat)) ||
+			(block.getRelative(BlockFace.DOWN).getType().equals(mat)) ||
+			(block.getRelative(BlockFace.EAST).getType().equals(mat)) ||
+			(block.getRelative(BlockFace.WEST).getType().equals(mat)) ||
+			(block.getRelative(BlockFace.NORTH).getType().equals(mat)) ||
+			(block.getRelative(BlockFace.SOUTH).getType().equals(mat)) ||
+			(block.getRelative(BlockFace.NORTH_EAST).getType().equals(mat)) ||
+			(block.getRelative(BlockFace.NORTH_WEST).getType().equals(mat)) ||
+			(block.getRelative(BlockFace.SOUTH_EAST).getType().equals(mat)) ||
+			(block.getRelative(BlockFace.SOUTH_WEST).getType().equals(mat)))
+			return true;
+				
+		return false;
 	}
 
 	public void cuboidtest(Block block)
 	{
-		int h = 4; //number should be h-1 to match h value
-		
-		//Important note! X,Y,Z is a 3d directional, so, x and z operate on a flat plane not x and y. DONT FORGET IT AGAIN.
-		
-		Block blockmin1 = block.getWorld().getBlockAt(block.getX(), block.getY()-1, block.getZ()); //note y-1!
+		int h = 3; //number should be h-1 to match h value
+					
+		Block blockmin1 = block.getWorld().getBlockAt(block.getX()+h, block.getY()+h, block.getZ()+h); //note y-1!
 		Location x1 = blockmin1.getLocation();
-		Block blockmax1 = block.getWorld().getBlockAt(block.getX()+(h), block.getY()-1, block.getZ()+(h));
+		Block blockmax1 = block.getWorld().getBlockAt(block.getX()-h, block.getY()-h, block.getZ()-h);
 		Location y1 = blockmax1.getLocation();
 		
-		Block blockmin2 = block.getWorld().getBlockAt(block.getX(), block.getY()-1, block.getZ()); //note y-1!
-		Location x2 = blockmin2.getLocation();
-		Block blockmax2 = block.getWorld().getBlockAt(block.getX()-(h), block.getY()-1, block.getZ()-(h));
-		Location y2 = blockmax2.getLocation();
+		//invalidBlocks.add(block);
+		isTouching(block);
+		plugin.output(Integer.toString(invalidBlocks.size()));
 		
 		this.loopThrough(block, x1, y1);
-		this.loopThrough(block, x2, y2);
-		
+
 	}
 	
 }
