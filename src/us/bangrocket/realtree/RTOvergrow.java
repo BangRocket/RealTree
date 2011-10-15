7eec34aefb01370a3ec0bbf6ee3b0a83a369668b
Joshua Heidorn <heidorjm@live.com>
2011-10-14 21:56:46 -0400

    Revert 13c6b6e9e01442c3801446513f4249df9117f3e4^..HEAD

--- src/us/bangrocket/realtree/RTOvergrow.java
+++ src/us/bangrocket/realtree/RTOvergrow.java
@@ -1,5 +1,6 @@
 package us.bangrocket.realtree;
 
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 
@@ -11,50 +12,52 @@ public class RTOvergrow
 	{
         plugin = instance;
 	}
-
-	public void cuboidtest(Block block) //block is the bottom log/sapling block
+	
+	private void loopThrough(Block block, Location loc1, Location loc2)
 	{
-		int maxR = 8; //temp radius value (values from LF config)
-		int minR = 2; 
-		
-		int originalX = block.getX(); //master varis
-		int originalY = block.getY();
-		int originalZ = block.getZ();
-		
-		int tempX, tempY, tempZ ; //looping varis
- 		
- 		int diffX, diffY, diffZ; //difference varis
- 		
- 		int slopeDistance = 0;
-		
-		for(int x = 0; x<=(originalX+(maxR^2)); x++)
+	    int minx = Math.min(loc1.getBlockX(), loc2.getBlockX()),
+	        miny = Math.min(loc1.getBlockY(), loc2.getBlockY()),
+	        minz = Math.min(loc1.getBlockZ(), loc2.getBlockZ()),
+	        maxx = Math.max(loc1.getBlockX(), loc2.getBlockX()),
+	        maxy = Math.max(loc1.getBlockY(), loc2.getBlockY()),
+	        maxz = Math.max(loc1.getBlockZ(), loc2.getBlockZ());
+	    
+	    for(int x = minx; x<=maxx;x++)
 	    {
-			tempX = (originalX - x);
-			tempY = originalY;
-			for(int y = 0; y<=(originalY+(maxR^2)); y++)
+	        for(int y = miny; y<=maxy;y++)
 	        {
-				tempZ = originalZ;
-				tempY = (originalY - y);
-	            for(int z = 0; z<=(originalZ+(maxR^2)); z++)
+	            for(int z = minz; z<=maxz;z++)
 	            {
-	            	tempZ = (originalZ - 2);
-	            	diffX = tempX - originalX;
-	            	diffY = tempY - originalY;
-	            	diffZ = tempZ - originalZ;
-	            	
-	            	slopeDistance = (diffX^2) + (diffY^2) + (diffZ^2); 
-	            	
-	            	//if ((slopeDistance <= (maxR^2)) && (slopeDistance >= (minR^2)))
-	            	{
-	            		Block tempBlock = block.getWorld().getBlockAt(tempX, tempY, tempZ);
-	            		
-	            		tempBlock.setType(Material.BEDROCK);
-	            		
-	            	}
+	            	Block test = block.getWorld().getBlockAt(x, y, z);
+	                if (test.getType().equals(Material.GRASS) || test.getType().equals(Material.DIRT))
+	                	test.setType(Material.AIR);
+	                else
+	                	test.setType(Material.GRASS);
 	            }
 	        }
 	    }
+	    block.setType(Material.LOG);
+	}
 
+	public void cuboidtest(Block block)
+	{
+		int h = 4; //number should be h-1 to match h value
+		
+		//Important note! X,Y,Z is a 3d directional, so, x and z operate on a flat plane not x and y. DONT FORGET IT AGAIN.
+		
+		Block blockmin1 = block.getWorld().getBlockAt(block.getX(), block.getY()-1, block.getZ()); //note y-1!
+		Location x1 = blockmin1.getLocation();
+		Block blockmax1 = block.getWorld().getBlockAt(block.getX()+(h), block.getY()-1, block.getZ()+(h));
+		Location y1 = blockmax1.getLocation();
+		
+		Block blockmin2 = block.getWorld().getBlockAt(block.getX(), block.getY()-1, block.getZ()); //note y-1!
+		Location x2 = blockmin2.getLocation();
+		Block blockmax2 = block.getWorld().getBlockAt(block.getX()-(h), block.getY()-1, block.getZ()-(h));
+		Location y2 = blockmax2.getLocation();
+		
+		this.loopThrough(block, x1, y1);
+		this.loopThrough(block, x2, y2);
+		
 	}
 	
 }
